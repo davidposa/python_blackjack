3cdcb30a2948fe7ef6e834b7586e8c6ac31c2c35
david <davidpoval89@gmail.com>
2024-03-21 02:16:49 +0000

    funcion para unir strings linea por linea

---
+++ otros.py
@@ -0,0 +1,9 @@
+""" Funciones varias
+"""
+def unir_str_por_linea(str1: str, str2: str) -> str:
+    """ Funcion para unir dos strings linea por linea
+    :param str1: Primer string para unir
+    :param str2: Segundo string para unir
+    :return: La union de los dos string en el input
+    """
+    return "\n".join([x1 + x2 for x1, x2 in zip(*list(map(lambda x: x.split("\n"), [str1, str2])))])
