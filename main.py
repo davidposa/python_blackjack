e487e058a613ed57d35b589e816f746cbff46ad1
david <davidpoval89@gmail.com>
2024-03-21 05:56:41 +0000

    inicio desarrollo de logica principal

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 from externo import Mazo, Estrategia
 from carta import Carta
+from mano import Mano
+
+
+def jugar_partida(mazo: Mazo, estrategia: Estrategia, balance: int, num_part: int):
+    print(F"--- INICIO DE LA PARTIDA #{num_part} --- BALANCE = {balance} €")
+    apuesta = input("¿Apuesta? [2] [10] [50]: ")
+    while apuesta not in ["2", "10", "50", ""]:
+        apuesta = input("¿Apuesta? [2] [10] [50]: ")
+    apuesta = 10 if apuesta == "" else int(apuesta)
+
+    print("\nREPARTO INICIAL")
+    mano_cr = Mano("Croupier", apuesta, cartas=[mazo.reparte()])
+    mano_jug = Mano("Mano", apuesta, cartas=[mazo.reparte(), mazo.reparte()])
+    print(str(mano_cr))
+    print(str(mano_jug))
+
+    if mano_jug.valor == 21:
+        output = apuesta * (3/2)
+        print("*****************\n*** BLACKJACK ***\n*****************\n")
+        print(f"Ha ganado {output} €!")
+        return output
 
 
 def main():
     estrategia = Estrategia(Mazo.NUM_BARAJAS)
     mazo = Mazo(Carta, estrategia)
+    balance = 0
+
+    print("*** BLACKJACK - PARADIGMAS DE PROGRAMACIÓN 2023/24 ***")
+    modo = input("¿Modo de ejecución? [J]uego [A]nálisis: ")
+    while modo.lower() not in ["j", "a", ""]:
+        modo = input("¿Modo de ejecución? [J]uego [A]nálisis: ")
+
+    # modo Juego se ejecuta por defecto si el usuario le da a enter
+    if modo.lower() in ["j", ""]:
+        num_part = 1
+        otra = "s"
+        while otra.lower() in ["s", ""]:
+            balance += jugar_partida(mazo, estrategia, balance, num_part)
+            otra = input("¿Otra partida? [S/N]: ")
+            while otra.lower() not in ["s", "n", ""]:
+                otra = input("¿Otra partida? [S/N]: ")
+        print(f"BALANCE FINAL: {balance} €")
 
 
 if __name__ == "__main__":
